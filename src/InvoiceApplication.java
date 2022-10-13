fae1df960149fbe8d43b58c3a4e00da03dc2e4af
ntuff <ndtuff@gmail.com>
2022-10-13 12:21:51 -0230

    .

--- src/InvoiceApplication.java
+++ src/InvoiceApplication.java
@@ -16,58 +16,93 @@ public class InvoiceApplication {
         Invoice invoice = new Invoice(lineItem1, lineItem2, lineItem3);
 
         System.out.println("Invoice Application");
-    } // remove
 
-//        Scanner input = new Scanner(System.in);
-//        char c; //Char to drive menu choice
-//
-//        do{
-//
-//            printMenu();
-//            //Grab the user input
-//            c = Character.toUpperCase(input.next().charAt(0));
-//
-//            System.out.printf("\nEcho: %c", c);
-//
-//            if(c=='A'){
-//                System.out.printf("\nIn Option %c\n", c);
-//                System.out.println("Enter deposit amount:");
-//
-//                int depositAmount = input.nextInt();
-//                account.credit(depositAmount);
-//
-//                printAccountInfo(account);
-//
-//            } else  if(c=='B'){
-//                System.out.printf("\nIn Option %c\n", c);
-//                System.out.println("Enter deposit amount:");
-//
-//                int withdrawalAmount = input.nextInt();
-//                account.debit(withdrawalAmount);
-//
-//                printAccountInfo(account);
-//
-//            } else if (c=='C'){
-//                System.out.printf("\nIn Option %c", c);
-//                account.reset();
-//
-//                printAccountInfo(account);
-//            }
-//
-//        }while(c!='Q');
-//
-//        System.out.println("\nGOODBYE!");
-//    }
-//
-//    public static void printMenu(){
-//        System.out.println("\n\nPlease select an option:");
-//        System.out.println("(A) Deposit funds");
-//        System.out.println("(B) Withdraw funds");
-//        System.out.println("(C) Reset balance");
-//        System.out.println("(Q) Quit");
-//    }
-//
-//    public static void printAccountInfo(Account account){
-//        System.out.printf("Account name: %s\nBalance: $%.2f", account.getName(), account.getBalance());
-//    }
+        Scanner input = new Scanner(System.in);
+        char c; //Char to drive menu choice
+        char c2; //Char to drive sub menu choice
+        do {
+
+            printMenu();
+            //Grab the user input
+            c = Character.toUpperCase(input.next().charAt(0));
+
+            if (c == 'A') {
+                System.out.printf("\nOption %c\n------------", c);
+                System.out.println("Please select an option:");
+                do {
+
+                    printSubMenu();
+                    c2 = Character.toUpperCase(input.next().charAt(0));
+
+                    if (c2 == 'A') {
+                        System.out.println("Enter new product ID:");
+                        product1.setId(input.nextInt());
+
+                    } else if (c2 == 'B') {
+                        System.out.println("Enter new product description:");
+                        product1.setDescription(input.next());
+
+                    } else if (c2 == 'C') {
+                        System.out.println("Enter new product price:");
+                        product1.setPrice(input.nextDouble());
+
+                    } else if (c2 == 'D') {
+                        printProductInfo(product1);
+
+                    } else if (c2 == 'E') {
+                        printLineItemInfo(lineItem1);
+                    }
+
+                }while (c2 != 'R');
+
+                invoice.getInvoiceTotal();
+
+            } else if (c == 'B') {
+                System.out.printf("\nIn Option %c\n", c);
+                System.out.println("Enter tbdvalue:");
+
+                //int withdrawalAmount = input.nextInt();
+                //account.debit(withdrawalAmount);
+
+                //printAccountInfo(account);
+
+            } else if (c == 'C') {
+                System.out.printf("\nIn Option %c", c);
+                //account.reset();
+
+                //printAccountInfo(account);
+
+            } else if (c == 'D') {
+                System.out.printf("\nIn Option %c", c);
+                //account.reset();
+            }
+        }while (c != 'Q');
+
+            System.out.println("\nGOODBYE!");
+    }
+    public static void printMenu () {
+        System.out.println("\n\nPlease select an option:");
+        System.out.println("(A) First Line Item");
+        System.out.println("(B) Second Line Item");
+        System.out.println("(C) Third Line Item");
+        System.out.println("(Q) Quit");
+    }
+
+    public static void printSubMenu () {
+        System.out.println("\n\nPlease select an option:");
+        System.out.println("(A) Change product ID");
+        System.out.println("(B) Change product description");
+        System.out.println("(C) Change product price");
+        System.out.println("(D) View product details");
+        System.out.println("(E) View line item");
+        System.out.println("(R) Return");
+    }
+
+    public static void printLineItemInfo (LineItem lineItem){
+        System.out.printf("Product: %s\n Quantity: %d\n Total: $%.2f\n\n", lineItem.getProduct(), lineItem.getQuantity(), lineItem.getTotal());
+    }
+
+    public static void printProductInfo(Product product){
+        System.out.printf("Product ID: %d\nDescription: %s\nPrice: $%.2f\n\n", product.getId(), product.getDescription(), product.getPrice());
+    }
 }
